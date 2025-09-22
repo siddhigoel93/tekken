8311af1393b0c8c1e702583a14afce5375866518
TarunTalan <taruntalan12@gmail.com>
2025-09-22 17:38:13 +0530

    add x and y direction motion to main-char

--- script.js
+++ script.js
@@ -3,9 +3,44 @@ const ctx = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+class Sprite {
+    constructor(position, velocity) {
+        this.position = position;
+        this.velocity = velocity;
+    }
+
+    update() {
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+
+        if (this.position.x + spriteWidth >= innerWidth) {
+            this.position.x = innerWidth-spriteWidth;
+            this.velocity.x = 0;
+        }
+        if (this.position.x < 0) {
+            this.position.x = 0;
+            this.velocity.x = 0;
+        }
+
+        if (this.position.y + spriteHeight >= innerHeight) {
+            this.position.y = innerHeight - spriteHeight;
+            this.velocity.y = 0;
+        }
+        if (this.position.y < 0) {
+            this.position.y = 0;
+            this.velocity.y = 0;
+        }
+    }
+}
+
+const player = new Sprite(
+    { x: 100, y: 200 },
+    { x: -1, y: 1 } 
+);
 
 const playerImage = new Image();
 playerImage.src = "./resources/Idle.png";
+
 const spriteWidth = 160;
 const spriteHeight = 111;
 let gameFrame = 0;
@@ -14,12 +49,19 @@ let staggerFrames = 5;
 function animate() {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, innerWidth, innerHeight);
-    let position = Math.floor(gameFrame / staggerFrames) % 8;
-    let FrameX = spriteWidth *position;
-    let FrameY = spriteHeight*0;
-    ctx.drawImage(playerImage, FrameX, FrameY , spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+
+    let positionX = Math.floor(gameFrame / staggerFrames) % 8;
+    let frameX = spriteWidth * positionX;
+    let frameY = 0;
+
+    ctx.drawImage(
+        playerImage,
+        frameX, frameY, spriteWidth, spriteHeight,
+        player.position.x, player.position.y, spriteWidth, spriteHeight
+    );
+
+    player.update();
     gameFrame++;
-
 }
-animate();
 
+animate();
