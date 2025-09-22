701d24ed1b6c7a005092ccf68486a1ccfdd077d0
TarunTalan <taruntalan12@gmail.com>
2025-09-22 13:09:04 +0530

    add basic sprite animation to our main char

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+
+const playerImage = new Image();
+playerImage.src = "./resources/Idle.png";
+const spriteWidth = 160;
+const spriteHeight = 111;
+let gameFrame = 0;
+let staggerFrames = 5;
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    let position = Math.floor(gameFrame / staggerFrames) % 8;
+    let FrameX = spriteWidth *position;
+    let FrameY = spriteHeight*0;
+    ctx.drawImage(playerImage, FrameX, FrameY , spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    gameFrame++;
+
+}
+animate();
 
