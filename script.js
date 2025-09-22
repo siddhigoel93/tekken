5d9289221e2637155911e5358407a1efbe409c57
TarunTalan <taruntalan12@gmail.com>
2025-09-22 20:45:11 +0530

    add basic controls of player

--- script.js
+++ script.js
@@ -4,7 +4,50 @@ canvas.width = 1024;
 canvas.height = 576;
 
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-const gravity = 0.2;
+const gravity = 0.5;
+
+const key = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+let lastKey;
+addEventListener('keydown', function (event) {
+    switch (event.key) {
+        case 'd':
+        case 'ArrowRight':
+            key.d.pressed = true;
+            lastKey = 'd';
+            break;
+        case 'a':
+        case 'ArrowLeft':
+            key.a.pressed = true;
+            lastKey = 'a';
+            break;
+        case 'w':
+        case 'ArrowUp':
+            player.velocity.y = -10;
+            break;
+    }
+}
+)
+addEventListener('keyup', function (event) {
+    switch (event.key) {
+        case 'd':
+        case 'ArrowRight':
+            key.d.pressed = false;
+            break;
+        case 'a':
+        case 'ArrowLeft':
+            key.a.pressed = false;
+            break;
+    }
+}
+)
 
 class Sprite {
     constructor({ position, velocity }) {
@@ -20,6 +63,7 @@ class Sprite {
 
     update() {
         this.draw();
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
         if (this.position.y + this.height >= canvas.height) {
             this.velocity.y = 0;
@@ -45,6 +89,13 @@ function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
     enemy.update();
+
+    player.velocity.x = 0;
+    if (key.a.pressed && lastKey == 'a') {
+        player.velocity.x = -1;
+    } else if (key.d.pressed && lastKey == 'd') {
+        player.velocity.x = 1;
+    }
     requestAnimationFrame(animate);
 }
 animate();
