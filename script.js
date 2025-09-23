25dd839e18cc9ca0adbff9657ef4f56c64173ec8
Siddhi Goel <siddhigoel93@gmail.com>
2025-09-23 13:03:48 +0530

    changed position of characters

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ class Fighter {
         this.draw();
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
-        if (this.position.y + this.height >= canvas.height) {
+        if (this.position.y + this.height >= canvas.height-90) {
             this.velocity.y = 0;
         } else {
             this.velocity.y += gravity;
